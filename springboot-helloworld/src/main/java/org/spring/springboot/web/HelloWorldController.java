b0405d9d31c979604cbbfa051c6a5bc42ddd220c
csj2 <1770700258@qq.com>
2020-09-13 13:37:03 +0800

    1

--- springboot-helloworld/src/main/java/org/spring/springboot/web/HelloWorldController.java
+++ springboot-helloworld/src/main/java/org/spring/springboot/web/HelloWorldController.java
@@ -1,5 +1,8 @@
 package org.spring.springboot.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/")
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+    @Value("active")
+    private String value;
+
+    @RequestMapping("/hello")
     public String sayHello() {
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.warn("warn...");
+        logger.error("error...");
         return "Hello,World!";
     }
 }
